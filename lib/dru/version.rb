8287037e8cbeba2a9704fdce410d2fd14544103c
Steven Fernandez <sfernandez0201@gmail.com>
2023-11-04 20:11:34 -0400

    first commit

---
+++ lib/dru/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dru
+  VERSION = "0.1.0"
+end
---
+++ test/test_dru.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestDru < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Dru::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
